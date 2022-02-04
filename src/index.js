bc25e8e5c7b4a2942838c6cdbc54a2711504a9ba
Patrik Janosdeak <patrik.janosdeak@isobar.com>
2022-02-04 10:42:29 +0100

    Add option to validate specific outputCategory if supplied + test coverage for this function

--- src/index.js
+++ src/index.js
@@ -1,20 +1,54 @@
-module.exports = function isResize(value) {
+const outputCategories = ['html', 'image', 'print', 'video', 'audio', 'fallback'];
+const audioRegex = [
+	// audio size
+	/^(\d+)s(?:@([a-z0-9-]+))?(?<!-)$/g,
+];
+const printRegex = [
+	// responsive print size
+	/^(\d+)-(\d+)x(\d+)-(\d+)mm(?:@([a-z0-9-]+))?(?<!-)$/g,
+	// print size
+	/^(\d+)x(\d+)mm(?:@([a-z0-9-]+))?(?<!-)$/g,
+];
+const sizeRegex = [
+	// responsive size
+	/^(\d+)-(\d+)x(\d+)-(\d+)(?:@([a-z0-9-]+))?(?<!-)$/g,
+	// regular size
+	/^(\d+)x(\d+)(?:@([a-z0-9-]+))?(?<!-)$/g
+];
+
+module.exports = function isResize(value, outputCategory = null) {
 	if (typeof value !== 'string') {
 		throw new Error('Supplied value must be a string.');
 	}
 
-	const regexes = [
-		// audio size
-		/^(\d+)s(?:@([a-z0-9-]+))?(?<!-)$/g,
-		// responsive print size
-		/^(\d+)-(\d+)x(\d+)-(\d+)mm(?:@([a-z0-9-]+))?(?<!-)$/g,
-		// print size
-		/^(\d+)x(\d+)mm(?:@([a-z0-9-]+))?(?<!-)$/g,
-		// responsive size
-		/^(\d+)-(\d+)x(\d+)-(\d+)(?:@([a-z0-9-]+))?(?<!-)$/g,
-		// regular size
-		/^(\d+)x(\d+)(?:@([a-z0-9-]+))?(?<!-)$/g
-	];
+	if (outputCategory && typeof outputCategory !== 'string') {
+		throw new Error('Supplied outputCategory must be a string.');
+	}
+
+	if (outputCategory && !outputCategories.includes(outputCategory)) {
+		throw new Error('Supplied outputCategory must be a valid output category.');
+	}
+
+	let regexes = [];
+
+	switch (outputCategory) {
+		case 'audio':
+			regexes.push(...audioRegex);
+			break;
+		case 'print':
+			regexes.push(...printRegex);
+			break;
+		case 'html':
+		case 'image':
+		case 'video':
+		case 'fallback':
+			regexes.push(...sizeRegex);
+			break;
+		case null:
+		default:
+			regexes.push(...audioRegex, ...printRegex, ...sizeRegex);
+			break;
+	}
 
 	let matches = [];
 
--- tests/isResize.test.js
+++ tests/isResize.test.js
@@ -4,16 +4,62 @@ test('it should be a valid resize', () => {
 	expect(isResize('300x300')).toBe(true);
 });
 
-test('it should not be a valid resize', () => {
+test('it should be an invalid resize', () => {
 	expect(isResize('cic')).toBe(false);
 });
 
-test('it should throw an error if anything other than a string is sent as a', () => {
+test('it should be a valid audio resize', () => {
+	expect(isResize('20s', 'audio')).toBe(true);
+});
+
+test('it should be an invalid audio resize', () => {
+	expect(isResize('300x300', 'audio')).toBe(false);
+
+	expect(isResize('cic', 'audio')).toBe(false);
+});
+
+test('it should be a valid print resize', () => {
+	expect(isResize('210x297mm', 'print')).toBe(true);
+});
+
+test('it should be an invalid print resize', () => {
+	expect(isResize('300x300', 'print')).toBe(false);
+
+	expect(isResize('cic', 'print')).toBe(false);
+});
+
+test('it should be a valid html, image, video or fallback resize', () => {
+	['html', 'image', 'video', 'fallback'].forEach((outputCategory) => {
+		expect(isResize('300x300', outputCategory)).toBe(true);
+	});
+});
+
+test('it should be an invalid html, image, video or fallback resize', () => {
+	expect(isResize('210z297mm', 'html')).toBe(false);
+
+	expect(isResize('20s', 'video')).toBe(false);
+});
+
+test('it should throw an error if value is not a string', () => {
 	expect(() => isResize({})).toThrowError(new Error('Supplied value must be a string.'));
 
-	expect(() => isResize(() => console.log('cic'))).toThrowError(new Error('Supplied value must be a string.'));
+	expect(() => isResize(() => console.log('cic')))
+		.toThrowError(new Error('Supplied value must be a string.'));
 
 	expect(() => isResize(undefined)).toThrowError(new Error('Supplied value must be a string.'));
 
 	expect(() => isResize(NaN)).toThrowError(new Error('Supplied value must be a string.'));
+});
+
+test('it should throw an error if outputCategory is not a string', () => {
+	expect(() => isResize('300x300', {}))
+		.toThrowError(new Error('Supplied outputCategory must be a string.'));
+
+	expect(() => isResize('300x300', () => console.log('cic')))
+		.toThrowError(new Error('Supplied outputCategory must be a string.'));
+});
+
+test('it should throw an error if outputCategory is not a valid outputCategory', () => {
+	expect(() => isResize('300x300', 'cic'))
+		.toThrowError(new Error('Supplied outputCategory must be a valid output category.'));
 });
